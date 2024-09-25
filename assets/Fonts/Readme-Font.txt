b2770ebeea376a5255d52549e8b39cce5a340ff0
Brad Hannah <47454550+bradhannah@users.noreply.github.com>
2024-09-24 21:22:31 -0400

    feat: right side borders

--- assets/Fonts/Readme-Font.txt
+++
@@ -1,1 +0,0 @@
-Avatar.ttf downloaded from http://www.thealmightyguru.com/GameFonts/Series-Ultima.html
--- cmd/ultimav/game_scene.go
+++ cmd/ultimav/game_scene.go
@@ -16,13 +16,20 @@ const (
 	borderWidthScaling = 601
 	xTilesInMap        = 19
 	yTilesInMap        = 14
+	keyPressDelay      = 175
 )
 
 type gameBorders struct {
-	outsideBorder     *ebiten.Image
-	outsideBorderOp   *ebiten.DrawImageOptions
-	rightSideBorder   *ebiten.Image
-	rightSideBorderOp *ebiten.DrawImageOptions
+	outsideBorder                 *ebiten.Image
+	outsideBorderOp               *ebiten.DrawImageOptions
+	rightSideBorder               *ebiten.Image
+	rightSideBorderOp             *ebiten.DrawImageOptions
+	topRightCharacterBorder       *ebiten.Image
+	topRightCharacterBorderOp     *ebiten.DrawImageOptions
+	midRightTextOutputBorder      *ebiten.Image
+	midRightTextOutputBorderOp    *ebiten.DrawImageOptions
+	bottomRightProvisionsBorder   *ebiten.Image
+	bottomRightProvisionsBorderOp *ebiten.DrawImageOptions
 }
 
 // GameScene is another scene (e.g., the actual game)
@@ -69,23 +76,15 @@ func (g *GameScene) Update(game *Game) error {
 
 // Draw method for the GameScene
 func (g *GameScene) Draw(screen *ebiten.Image) {
-	screen.DrawImage(g.borders.rightSideBorder, g.borders.rightSideBorderOp)
-	screen.DrawImage(g.borders.outsideBorder, g.borders.outsideBorderOp)
-
-	// make unscaled map image
-	//do := ebiten.DrawImageOptions{}
-
-	//const xTiles = 20
-	//const yTiles = 14
+	g.drawBorders(screen)
 
 	mapImage := ebiten.NewImage(sprites.TileSize*xTilesInMap, sprites.TileSize*yTilesInMap)
-
 	g.drawMap(mapImage)
 	g.drawMapUnits(mapImage)
 
 	op := sprites.GetDrawOptionsFromPercents(mapImage, sprites.PercentBasedPlacement{
 		StartPercentX: .015,
-		EndPercentX:   0.75, //(.98 - 0.015) / (16.0 / 9.0),
+		EndPercentX:   0.75,
 		StartPercentY: 0.02,
 		EndPercentY:   0.98,
 	})
@@ -95,6 +94,48 @@ func (g *GameScene) Draw(screen *ebiten.Image) {
 	// Render the game scene
 	ebitenutil.DebugPrint(screen, g.debugMessage)
 
+}
+
+func (g *GameScene) initializeBorders() {
+	mainBorder := sprites.NewBorderSprites()
+	g.borders.outsideBorder, g.borders.outsideBorderOp = mainBorder.VeryPixelatedRoundedBlueWhite.CreateSizedAndScaledBorderSprite(borderWidthScaling, sprites.PercentBasedPlacement{
+		StartPercentX: 0,
+		StartPercentY: 0,
+		EndPercentY:   1,
+		EndPercentX:   1,
+	})
+
+	rightSideBorder := sprites.NewBorderSprites()
+	g.borders.rightSideBorder, g.borders.rightSideBorderOp = rightSideBorder.VeryPixelatedRoundedBlueWhite.CreateSizedAndScaledBorderSprite(borderWidthScaling, sprites.PercentBasedPlacement{
+		StartPercentX: .75,
+		EndPercentX:   1,
+		StartPercentY: 0,
+		EndPercentY:   1,
+	})
+
+	topRightCharacterBorder := sprites.NewBorderSprites()
+	g.borders.topRightCharacterBorder, g.borders.topRightCharacterBorderOp = topRightCharacterBorder.VeryPixelatedRoundedBlueWhite.CreateSizedAndScaledBorderSprite(borderWidthScaling, sprites.PercentBasedPlacement{
+		StartPercentX: .751,
+		EndPercentX:   1,
+		StartPercentY: 0.00,
+		EndPercentY:   0.5,
+	})
+
+	midRightTextOutputBorder := sprites.NewBorderSprites()
+	g.borders.midRightTextOutputBorder, g.borders.midRightTextOutputBorderOp = midRightTextOutputBorder.VeryPixelatedRoundedBlueWhite.CreateSizedAndScaledBorderSprite(borderWidthScaling, sprites.PercentBasedPlacement{
+		StartPercentX: .751,
+		EndPercentX:   1,
+		StartPercentY: .485,
+		EndPercentY:   0.8,
+	})
+
+	bottomRightTextOutputBorder := sprites.NewBorderSprites()
+	g.borders.bottomRightProvisionsBorder, g.borders.bottomRightProvisionsBorderOp = bottomRightTextOutputBorder.VeryPixelatedRoundedBlueWhite.CreateSizedAndScaledBorderSprite(borderWidthScaling, sprites.PercentBasedPlacement{
+		StartPercentX: .751,
+		EndPercentX:   1,
+		StartPercentY: 0.8 - .015,
+		EndPercentY:   1,
+	})
 }
 
 func NewGameScene(gameConfig *config.UltimaVConfiguration) *GameScene {
@@ -107,33 +148,24 @@ func NewGameScene(gameConfig *config.UltimaVConfiguration) *GameScene {
 		log.Fatal(err)
 	}
 
-	// initialize borders
-	mainBorder := sprites.NewBorderSprites()
-	image, op := mainBorder.VeryPixelatedRoundedBlueWhite.CreateSizedAndScaledBorderSprite(borderWidthScaling, sprites.PercentBasedPlacement{
-		StartPercentX: 0,
-		StartPercentY: 0,
-		EndPercentY:   1,
-		EndPercentX:   1,
-	})
-	gameScene.borders.outsideBorder = image
-	gameScene.borders.outsideBorderOp = op
-	rightSideBorder := sprites.NewBorderSprites()
-	image, op = rightSideBorder.VeryPixelatedRoundedBlueWhite.CreateSizedAndScaledBorderSprite(borderWidthScaling, sprites.PercentBasedPlacement{
-		StartPercentX: .75,
-		EndPercentX:   1,
-		StartPercentY: 0,
-		EndPercentY:   1,
-	})
-	gameScene.borders.rightSideBorder = image
-	gameScene.borders.rightSideBorderOp = op
+	gameScene.initializeBorders()
 
 	gameScene.spriteSheet = sprites.NewSpriteSheet()
 
-	gameScene.keyboard = &input.Keyboard{MillisecondDelayBetweenKeyPresses: 100}
+	gameScene.keyboard = &input.Keyboard{MillisecondDelayBetweenKeyPresses: keyPressDelay}
 	gameScene.avatarX = 75
 	gameScene.avatarY = 75
 
 	return &gameScene
+}
+
+// drawBorders
+func (g *GameScene) drawBorders(screen *ebiten.Image) {
+	screen.DrawImage(g.borders.rightSideBorder, g.borders.rightSideBorderOp)
+	screen.DrawImage(g.borders.outsideBorder, g.borders.outsideBorderOp)
+	screen.DrawImage(g.borders.topRightCharacterBorder, g.borders.topRightCharacterBorderOp)
+	screen.DrawImage(g.borders.midRightTextOutputBorder, g.borders.midRightTextOutputBorderOp)
+	screen.DrawImage(g.borders.bottomRightProvisionsBorder, g.borders.bottomRightProvisionsBorderOp)
 }
 
 // drawMapUnits
